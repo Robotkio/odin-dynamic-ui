ed141c932d409444f11f878ce198c3ad70c23a6b
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-07-21 19:08:12 -0700

    saving sttuff bbefforeworkingg anoth

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./css/style.css";
 
-console.log("Hello world!");
+const CLASS_NAME = "dropdown-menu"
+const HIDDEN_CLASS = "display-none"
+const DISPLAY_CLASS = "display-block"
+
+addMenuFunction()
+
+function addMenuFunction() {
+    const menus = document.querySelectorAll(`.${CLASS_NAME} ul`)
+    menus.forEach((menu) => {
+        menu.previousElementSibling.addEventListener("click", (menu) => {
+            let next = menu.target;
+            while(next = next.nextElementSibling) {
+                if (next.classList.contains(HIDDEN_CLASS)) {
+                    next.classList.remove(HIDDEN_CLASS)
+                    next.classList.add(DISPLAY_CLASS)
+                } else {
+                    next.classList.remove(DISPLAY_CLASS)
+                    next.classList.add(HIDDEN_CLASS)
+                }
+            }
+        })
+    })
+}
