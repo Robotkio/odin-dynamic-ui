9cd952990338ad75e5413d268e3dc1bfeeadfb34
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-07-31 22:29:01 -0700

    messing round

--- src/index.js
+++ src/index.js
@@ -4,22 +4,69 @@ const CLASS_NAME = "dropdown-menu"
 const HIDDEN_CLASS = "display-none"
 const DISPLAY_CLASS = "display-block"
 
-addMenuFunction()
+init()
 
-function addMenuFunction() {
-    const menus = document.querySelectorAll(`.${CLASS_NAME} ul`)
+function init() {
+    addMenuShowHideFunctionClassless()
+    addOnHoverOnOutFunction()
+
+    const menus = document.querySelectorAll(`.${CLASS_NAME}`)
+    for (const menu of menus) {
+        const elements = menu.children
+        for (const element of elements) {
+            hideAllDecendants(element)
+        }
+    }
+} 
+
+function addMenuShowHideFunctionClassless() {
+    const menus = document.querySelectorAll(`.${CLASS_NAME} ul, .${CLASS_NAME}`)
     menus.forEach((menu) => {
-        menu.previousElementSibling.addEventListener("click", (menu) => {
-            let next = menu.target;
-            while(next = next.nextElementSibling) {
-                if (next.classList.contains(HIDDEN_CLASS)) {
-                    next.classList.remove(HIDDEN_CLASS)
-                    next.classList.add(DISPLAY_CLASS)
-                } else {
-                    next.classList.remove(DISPLAY_CLASS)
-                    next.classList.add(HIDDEN_CLASS)
-                }
-            }
+        menu.addEventListener('click', (event) => {
+            displayAllChildren(event.target.nextElementSibling, 'block')
         })
     })
+}
+
+function addOnHoverOnOutFunction() {
+    const menus = document.querySelectorAll(`.${CLASS_NAME}`)
+    menus.forEach((menu) => {
+        menu.addEventListener("mouseenter", onMouseOver(menu))
+        menu.addEventListener("mouseleave", onMouseOut(menu))
+    })
+}
+
+function onMouseOver(element) {
+    return () => {
+        element.style['background-color'] = '#707070'
+    }
+}
+
+function onMouseOut(element) {
+    return () => {
+        element.style['background-color'] = null
+        const topEelements = element.children
+        let i = 0
+        while(topEelements[i]) {
+            hideAllDecendants(topEelements[i++])
+        }
+    }
+}
+
+function displayAllChildren(element, displayStyle) {
+    console.log(element.children)
+    const children = element.children
+    for (const child of children) {
+        console.log(child)
+        child.style.display = displayStyle
+    }
+}
+
+function hideAllDecendants(element) {
+    const children = element.querySelectorAll('ul')
+    console.log(children)
+    let i = 0;
+    while(children[i]) {
+        children[i++].style.display = 'none'
+    }
 }
